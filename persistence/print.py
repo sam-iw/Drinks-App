6257d2a9de5992a7b7a8927dacb7630c2c035daf
Sam <samuel.evans@infinityworks.com>
2020-04-22 14:09:21 +0100

    One fine week's work

--- persistence/print.py
+++ persistence/print.py
@@ -1,6 +1,13 @@
+def print_list(list): # Print list function
+    count = 1
+    for items in list:
+        print(f" {count} {items}")
+        count += 1
 
 
-
+def print_class_dict(dict):
+    for item in dict:
+        item.print()
 
 def print_list(list): # Print list function
     count = 1
--- persistence/save.py
+++ persistence/save.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def save_list(filename, lst):
     try:
         with open(filename, 'w') as file:
@@ -11,20 +12,30 @@ def save_list(filename, lst):
 
 def save_csv_dictionary(filepath, dictionary):
     try:
-        with open(filepath, "w") as csvfile: #pass csv file to the csv.writer
-            dictionary_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            for key, value in dictionary.items():# key and values in dict two, dict_two.items()
-                dictionary_file.writerow([key, value])
+        with open(filepath, "w") as csvfile:  # pass csv file to the csv.writer
+            dictionary_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            for data in dictionary.items():
+                dictionary_file.writerow(data)
     except:
-        print("An error occurred (sorry)")
+        print("An error occurred (sorry:CSV)")
 
+
+def save_hot_drinks(filepath, dictionary):
+    try:
+        with open(filepath, "w") as csvfile:  # pass csv file to the csv.writer
+            dictionary_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for person, value in dictionary.items():
+                dictionary_file.writerow([person, value[0], value[1], value[2], value[3]])
+    except Exception as error:
+        print(error)
+        print("An error occurred (sorry:CSV)")
 
 def save_dictionary(filepath, dictionary):
     try:
         with open(filepath, "w") as dictionary_file:
-            for key, value in dictionary.items():# key and values in dict two, dict_two.items()
-                dictionary_file.write(key, value) # add to the text file, saves it (I think)
+            for key, value in dictionary.items():  # key and values in dict two, dict_two.items()
+                dictionary_file.write(key, value)  # add to the text file, saves it (I think)
     except:
-        print("An error occurred (sorry)")
+        print("An error occurred (sorry:Dict)")
 
 
